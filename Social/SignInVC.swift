a5bfbf48fd8414b42e428eb7b96b73c0e64a3e47
Jamel Reid <jreid@qualityworkscg.com>
2017-04-30 11:06:56 -0500

    set up facebook sign in flow

--- Social/SignInVC.swift
+++ Social/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FacebookLogin
+import FacebookCore
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,37 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func FBBtnPressed(_ sender: UIButton) {
+        
+        let loginManager = LoginManager()
+        loginManager.logIn([ ReadPermission.publicProfile ], viewController: self) { loginResult in
+            switch loginResult {
+                case .failed(let error):
+                    print(error)
+                case .cancelled:
+                    print("User cancelled login to Facebook.")
+                case .success( _, _, let accessToken):
+                    print("Logged in to Facebook!")
+                    let credential = FIRFacebookAuthProvider.credential(withAccessToken: accessToken.authenticationToken)
+                    self.firebaseAuth(credential)
+            }
+        }
+        
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Unable to authenticate with Firebase - \(String(describing: error))")
+            } else {
+                print("Successfully Authenticated with Firebase")
+                if let userData = user {
+                    print(userData.uid)
+                    print(userData.displayName!)
+                }
+            }
+        })
+    }
 
 }
 
